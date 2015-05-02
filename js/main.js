8d089f5bee715285b3af55f2dbf3fd80b31d2cdf
Evan Carlson <pugdyuddly@gmail.com>
2015-05-02 18:27:37 -0500

    Added video and audio player apps

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+var cloudinatorVersion = 0.02;
+
 function osLoaded(){
+	// Initialize tooltips
+	$(document).tooltip();
+	// Load built-in applications
 	loadApp('system/apps/settings/settings.fax', 'settings', 'Settings');
+	loadApp('system/apps/player/audioplayer.fax', 'audioPlayer', 'Audio Player');
+	loadApp('system/apps/player/videoplayer.fax', 'videoPlayer', 'Video Player');
 }
